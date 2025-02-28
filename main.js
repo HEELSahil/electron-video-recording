d0cc4ddfd489201e4abdafd2a7e56df156a3d5b4
Sahil Kotian <sahil.kotian316@gmail.com>
2025-02-27 20:47:11 -0500

    Integrate Electron Into Project

---
+++ main.js
@@ -0,0 +1,23 @@
+// main.js
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  mainWindow.loadURL("http://localhost:3000");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
